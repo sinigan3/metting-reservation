7b3e682efd59fc64aea08658e81c7b6e5b4dccb3
Sini <Sinigan@chancetop.com>
2024-04-26 16:51:55 +0800

    feat: header增加返回首页

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { useSelector } from 'core-fe';
+import { useSelector, useLocation, NavLink } from 'core-fe';
 import type { State } from 'core-fe';
+import { Breadcrumb } from 'antd';
 
 type Props = {};
 
 export default function Header({}: Props) {
   const { username } = useSelector((state: State) => state.app.user.userInfo as IUser);
-  return <div>当前登录用户：{username}</div>;
+
+  console.log(1111, useLocation());
+  return (
+    <div>
+      <Breadcrumb>
+        <Breadcrumb.Item>
+          <NavLink to="/">{'<--'}返回会议室列表页</NavLink>
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      当前登录用户：{username}
+    </div>
+  );
 }
